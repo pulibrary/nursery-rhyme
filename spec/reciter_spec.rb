f1f804215ca53e8266d2cdc68c281f3a4d144f1d
zaymasood <zmasood@princeton.edu>
2021-07-01 16:42:59 -0400

    write a test example to return the string for THTJB (The House that Jack Built) rhyme

--- spec/reciter_spec.rb
+++ spec/reciter_spec.rb
@@ -2,8 +2,11 @@ require 'reciter'
 
 describe Reciter do
 
-  it '' do
-    expect()
+  let(:rhyme_path) { 'spec/fixtures/the_house_that_jack_built.txt' }
+
+  it 'returns a string for the rhyme "The House that Jack Built"' do
+    file = File.open(rhyme_path).read
+    expect(Reciter.to_s).to eq(file)
   end
 
 end

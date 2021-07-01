0550050e2f98ec45dbb9b957e3653ef4e29da15e
zaymasood <zmasood@princeton.edu>
2021-07-01 16:45:20 -0400

    update variable name

--- spec/reciter_spec.rb
+++ spec/reciter_spec.rb
@@ -5,8 +5,8 @@ describe Reciter do
   let(:rhyme_path) { 'spec/fixtures/the_house_that_jack_built.txt' }
 
   it 'returns a string for the rhyme "The House that Jack Built"' do
-    file = File.open(rhyme_path).read
-    expect(Reciter.to_s).to eq(file)
+    rhyme = File.open(rhyme_path).read
+    expect(Reciter.to_s).to eq(rhyme)
   end
 
 end

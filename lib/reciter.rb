9b786ff3bc6dbf5780981818128b0115e0422d07
zaymasood <zmasood@princeton.edu>
2021-07-02 15:57:56 -0400

    use nil when setting default parameter

--- lib/reciter.rb
+++ lib/reciter.rb
@@ -15,7 +15,9 @@ class Reciter
     "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
   end
 
-  def recite_random_rhyme(random_indices=generate_random_indices)
+  def recite_random_rhyme(random_indices=nil)
+    random_indices ||= generate_random_indices
+
     phrases = [
       "the house that Jack built",
       "the malt that lay in",

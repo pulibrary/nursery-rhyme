2a501cdb192e1d2944ea4ed1a51023ab98d27ec5
zaymasood <zmasood@princeton.edu>
2021-07-02 14:18:05 -0400

    create example for rhyme in random order

--- spec/reciter_spec.rb
+++ spec/reciter_spec.rb
@@ -3,10 +3,40 @@ require 'reciter'
 describe Reciter do
 
   let(:rhyme_path) { 'spec/fixtures/the_house_that_jack_built.txt' }
+  let(:rhyme_phrases_path) { 'spec/fixtures/the_house_that_jack_built_phrases.txt' }
 
-  it 'returns a string for the rhyme "The House that Jack Built"' do
-    rhyme = File.open(rhyme_path).read
-    expect(subject.read).to eq(rhyme)
+  describe '#read' do
+
+    it 'returns a string for the rhyme' do
+      rhyme = File.open(rhyme_path).read
+      expect(subject.read).to eq(rhyme)
+    end
+
   end
+
+  describe '#read_random' do
+
+    it 'returns a string for the rhyme with each element in random order' do
+      phrases = []
+      File.open(rhyme_phrases_path).each_line do |line|
+        phrases.push(line)
+      end
+
+      random_numbers = subject.generate_random_numbers
+      continous_phrase = ""
+      random_rhyme = ""
+      first = true
+
+      for number in random_numbers
+        continous_phrase = phrases[number].strip + " " + continous_phrase
+        random_rhyme += "This is " + continous_phrase.strip + ".\n"
+      end
+
+      expect(subject.read_random(random_numbers)).to eq(random_rhyme)
+    end
+
+  end
+
+
 
 end

f7001c6569512524104b5b8c16f2460d1b5989da
zaymasood <zmasood@princeton.edu>
2021-07-02 16:23:49 -0400

    shorten test

--- spec/reciter_spec.rb
+++ spec/reciter_spec.rb
@@ -18,25 +18,19 @@ describe Reciter do
 
     it 'returns a string for the rhyme with each element in random order' do
       phrases = File.open(rhyme_phrases_path).readlines
-      indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].shuffle
+      indices1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].shuffle
+      indices2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].shuffle
 
-      random_indices = indices.shuffle
+      random_indices1 = indices.shuffle
       continous_phrase = ""
       random_rhyme = ""
-      for random_index in random_indices
+      for random_index in random_indices1
         continous_phrase = phrases[random_index].strip + " " + continous_phrase
         random_rhyme += "This is " + continous_phrase.strip + ".\n"
       end
-      expect(subject.recite_random_rhyme(random_indices)).to eq(random_rhyme)
-
-      random_indices = indices.shuffle
-      continous_phrase = ""
-      random_rhyme = ""
-      for random_index in random_indices
-        continous_phrase = phrases[random_index].strip + " " + continous_phrase
-        random_rhyme += "This is " + continous_phrase.strip + ".\n"
-      end
-      expect(subject.recite_random_rhyme(random_indices)).to eq(random_rhyme)
+      
+      expect(subject.recite_random_rhyme(random_indices1)).to eq(random_rhyme)
+      expect(subject.recite_random_rhyme(random_indices2)).not_to eq(random_rhyme)
     end
 
   end
